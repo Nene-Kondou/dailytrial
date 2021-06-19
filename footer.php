dd2f438244be93938668d01005a3bd3d49d4c8ad
Nene-Kondou <gold.ne12@ezweb.ne.jp>
2021-06-19 15:19:54 +0900

    ファイル分割

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!-- footer -->
+<footer id="footer">
+		<div class="inner">
+			<div class="copy">&copy; daily-trial WordPress theme All rights reserved.</div><!-- /copy -->
+			<div class="by">Presented by <a href="https://tokyofreelance.jp/" rel="noopener" target="_blank">東京フリーランス</a>
+			</div><!-- /by -->
+
+		</div><!-- /inner -->
+	</footer><!-- /footer -->
+
+	<div class="floating">
+		<a href="#"><i class="fas fa-chevron-up"></i></a>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="js/script.js"></script>
+
+</body>
+
+</html>
